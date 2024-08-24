7070f4926dcf0b24cc20dee32d7b8d844d297fd6
MaterialMR <materialmr06@gmail.com>
2024-08-23 20:41:23 -0600

    Add files via upload

---
+++ READMEmd.txt
@@ -0,0 +1,7 @@
+echo "# Material-Did-ctico-MR" >> README.md 
+git init 
+git add README.md 
+git commit -m "primer commit" 
+git branch -M main 
+git remote add origin https://github.com/MaterialMR/Material-Did-ctico-MR.git
+ git push -u origin main
---
+++ script.js
@@ -0,0 +1,35 @@
+Ejemplo: La Voz de Tu Marca
+1. Tono y Voz
+Tono: Profesional, Amigable, Inspirador
+
+Voz: Nuestra marca habla de manera clara y accesible. Nos dirigimos a los docentes con respeto y comprensión, ofreciendo soluciones prácticas y efectivas que les inspiren y apoyen en su labor educativa.
+
+2. Mensajes Clave
+Compromiso con la Educación: "En Material Didáctico MR, estamos comprometidos con el éxito educativo de nuestros docentes y estudiantes."
+Soluciones Prácticas: "Ofrecemos herramientas didácticas prácticas y efectivas, diseñadas para facilitar el proceso de enseñanza y aprendizaje."
+Innovación y Creatividad: "Fomentamos la creatividad y la innovación en el aula, proporcionando recursos que inspiran y motivan a los estudiantes."
+3. Paleta de Colores y Tipografía
+Paleta de Colores:
+
+Primario: Azul (Confianza, profesionalismo)
+Secundario: Verde (Crecimiento, educación)
+Neutrales: Blanco y Gris (Claridad, simplicidad)
+Tipografía:
+
+Títulos: Roboto Slab (Claridad y modernidad)
+Cuerpo de Texto: Open Sans (Legibilidad y accesibilidad)
+Aplicación en un Folleto
+Portada
+Título: ¡Transforma tu Aula con Material Didáctico MR!
+Imagen: Una imagen inspiradora de un aula interactiva con estudiantes y docentes usando materiales didácticos.
+Mensaje: "Empodera a tus estudiantes con herramientas prácticas y efectivas."
+
+Interior del Folleto
+Sección 1: Quiénes Somos
+
+Texto: "En Material Didáctico MR, entendemos las necesidades de los docentes. Nos dedicamos a crear recursos didácticos que hacen la diferencia en el aula."
+Sección 2: Productos Destacados
+
+function myFunction() {
+    alert('Hola Mundo!');
+}
